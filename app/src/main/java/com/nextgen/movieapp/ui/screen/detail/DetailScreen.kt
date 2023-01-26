78c905b8f5fbfd0878b7475472e702e6c51f7aa0
ribakmasude <beranjusihombing1@gmail.com>
2023-01-27 02:19:05 +0700

    implement bottomsheetscaffold in detailscreen

--- app/src/main/java/com/nextgen/movieapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/nextgen/movieapp/ui/screen/detail/DetailScreen.kt
@@ -1,24 +1,40 @@
 package com.nextgen.movieapp.ui.screen.detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import com.nextgen.movieapp.R
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.nextgen.movieapp.BuildConfig
 import com.nextgen.movieapp.data.source.remote.response.DetailMovieResponse
 import com.nextgen.movieapp.data.source.remote.response.ResultsItem
 import com.nextgen.movieapp.ui.common.UiState
 import com.nextgen.movieapp.ui.component.DetailMainSection
 import com.nextgen.movieapp.ui.component.RateSection
+import com.nextgen.movieapp.ui.theme.Shapes
+import kotlinx.coroutines.launch
 
 @Composable
 fun DetailScreen(
@@ -44,7 +60,10 @@ fun DetailScreen(
                     Card(
                         shape = RoundedCornerShape(16.dp),
                         backgroundColor = MaterialTheme.colors.onPrimary,
-                        modifier = Modifier.padding(16.dp).size(300.dp).align(Alignment.Center)
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(300.dp)
+                            .align(Alignment.Center)
                     ) {
                         Text(
                             text = uiState.message,
@@ -57,33 +76,83 @@ fun DetailScreen(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DetailContent(
     data: DetailMovieResponse,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    Box(
+        modifier = modifier.fillMaxSize()
     ) {
-        DetailMainSection(
-            posterPath = "https://image.tmdb.org/t/p/original/${data.posterPath}",
-            title = data.title,
-            release = data.releaseDate,
-            language = data.originalLanguage,
-            age = data.adult,
-            navigateBack = navigateBack,
-        )
-        Divider()
-        RateSection(
-            voteCount = data.voteCount,
-            voteAverage = data.voteAverage,
-            popularity = data.popularity)
-        Divider()
-        Text(text = data.overview)
+
+        BottomSheetScaffold(
+            sheetContent = {
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowUp, 
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    RateSection(
+                        voteCount = data.voteCount,
+                        voteAverage = data.voteAverage,
+                        popularity = data.popularity,
+                    )
+                }
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(64.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Divider()
+                    Text(text = data.overview)
+                    Spacer(Modifier.height(20.dp))
+                    Button(
+                        onClick = {
+                            scope.launch { scaffoldState.bottomSheetState.collapse() }
+                        }
+                    ) {
+                        Text("Click to collapse sheet")
+                    }
+
+                }
+            },
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 200.dp,
+            ) {
+            Box(modifier = Modifier.padding(it)) {
+                AsyncImage(
+                    model = BuildConfig.IMAGE_BASE_URL+data.posterPath,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable { navigateBack() }
+                        .background(Color.LightGray, CircleShape)
+                )
+            }
+        }
 
     }
+
 
 }
